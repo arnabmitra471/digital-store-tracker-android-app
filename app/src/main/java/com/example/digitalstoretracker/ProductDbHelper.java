e8388535455d01f077c3cebe343211168836893d
Arnab Mitra <arnabmitra471@gmail.com>
2024-04-13 10:11:56 +0530

    configured the alert dialog when the add product button is clicked

--- app/src/main/java/com/example/digitalstoretracker/ProductDbHelper.java
+++ app/src/main/java/com/example/digitalstoretracker/ProductDbHelper.java
@@ -1,10 +1,11 @@
 package com.example.digitalstoretracker;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 
 public class ProductDbHelper extends SQLiteOpenHelper {
 
@@ -20,17 +21,56 @@ public class ProductDbHelper extends SQLiteOpenHelper {
     public static final String COL_PRODUCT_PRICE = "product_price";
     public static final String COL_PRODUCT_QTY = "product_qty";
 
-    public ProductDbHelper(@Nullable Context context) {
+    public ProductDbHelper(Context context) {
         super(context,DATABASE_NAME,null,DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String create = "CREATE TABLE IF NOT EXISTS "+TABLE_NAME+" ("+COL_PRODUCT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+COL_PRODUCT_NAME+" VARCHAR(50), "+COL_PRODUCT_PRICE+" VARCHAR(20), "+COL_PRODUCT_QTY+" VARCHAR(20) )";
+        db.execSQL(create);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String drop_products = "DROP TABLE IF EXISTS "+TABLE_NAME;
+        db.execSQL(drop_products);
+        onCreate(db);
+    }
+    public long addProduct(@NonNull ProductModel product)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues product_data = new ContentValues();
 
+        if(!product.getProduct_name().isEmpty());
+        {
+            product_data.put(COL_PRODUCT_NAME,product.getProduct_name());
+        }
+        if(!product.getProduct_price().isEmpty());
+        {
+            product_data.put(COL_PRODUCT_NAME,product.getProduct_price());
+        }
+        if(!product.getProduct_quantity().isEmpty());
+        {
+            product_data.put(COL_PRODUCT_NAME,product.getProduct_quantity());
+        }
+        long productInsertedId = db.insert(TABLE_NAME,null,product_data);
+        return productInsertedId;
+    }
+    public int updateProduct(String id,ProductModel product)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues uProductData = new ContentValues();
+        if(product.getProduct_name().isEmpty()) {
+            uProductData.put(COL_PRODUCT_NAME, product.getProduct_name());
+        }
+        if(product.getProduct_price().isEmpty()) {
+            uProductData.put(COL_PRODUCT_PRICE, product.getProduct_price());
+        }
+        if(product.getProduct_quantity().isEmpty()) {
+            uProductData.put(COL_PRODUCT_QTY, product.getProduct_quantity());
+        }
+        int updatedId = db.update(TABLE_NAME,uProductData,COL_PRODUCT_ID+" = ?",new String[]{id});
+        return updatedId;
     }
 }
--- app/src/main/java/com/example/digitalstoretracker/ProductListActivity.java
+++ app/src/main/java/com/example/digitalstoretracker/ProductListActivity.java
@@ -1,11 +1,9 @@
 package com.example.digitalstoretracker;
 
-import android.app.Activity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -18,7 +16,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
@@ -26,7 +23,7 @@ public class ProductListActivity extends AppCompatActivity {
 
     ListView productList;
     Button addProductBtn;
-    Activity context;
+
     ArrayList<ProductModel> allProducts = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +41,7 @@ public class ProductListActivity extends AppCompatActivity {
         ProductModel p2 = new ProductModel("Mustard Oil","130","100");
         ProductModel p3 = new ProductModel("Ghee","50","110");
         ProductModel p4 = new ProductModel("Chicken masala","50","86");
-        ProductModel p5 = new ProductModel("Biriyani masala","67","1000");
+        ProductModel p5 = new ProductModel("Biryani masala","67","1000");
         ProductModel p6 = new ProductModel("Eggs","5.5","180");
         ProductModel p7 = new ProductModel("Cardamom","25","100");
         ProductModel p8 = new ProductModel("Milk","45","1000");
@@ -83,13 +80,38 @@ public class ProductListActivity extends AppCompatActivity {
                         String product_price = productPriceInp.getText().toString().trim();
                         String product_qty = productQtyInp.getText().toString().trim();
 
+                        ProductDbHelper dbHelper = new ProductDbHelper(ProductListActivity.this);
                         if(product_name.isEmpty() || product_price.isEmpty() || product_qty.isEmpty())
                         {
                             Toast.makeText(ProductListActivity.this,"All fields are required",Toast.LENGTH_LONG).show();
                         }
+                        else
+                        {
+                            ProductModel product = new ProductModel(product_name,product_price,product_qty);
+                            long insertedId = dbHelper.addProduct(product);
+                            if(insertedId > 0)
+                            {
+                                Toast.makeText(ProductListActivity.this,"Product added successfully",Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                            {
+                                Toast.makeText(ProductListActivity.this,"Product was not added successfully",Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
 
                     }
                 });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.setCancelable(false);
+
+                AlertDialog alertProductDialog = builder.create();
+                alertProductDialog.show();
 
             }
         });
